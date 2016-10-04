d35392dde2569954490f1a6c99448b4351ef2c77
Louis Bakris <louis.bakris@gmail.com>
2016-10-03 19:07:05 -0500

    git init

---
+++ Name2.py
@@ -0,0 +1,18 @@
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+for key, data in users.items():
+    i=0
+    print key
+    for value in data:
+        i=i+1
+        print i,value['first_name'],value['last_name'],len(value['first_name']+value['last_name'])
